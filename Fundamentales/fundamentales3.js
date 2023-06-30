0525952b9c12722883e3215d31a3880fd059bd15
nathy <nathy@spartandevs.com>
2023-06-30 00:58:26 -0400

    se actualizan ejercicios

---
+++ Fundamentales/fundamentales3.js
@@ -0,0 +1,10 @@
+/*Ejercicio 3:
+Escribe una función que tome un valor como argumento. Devuelve el tipo del valor.*/
+const myFunction = (a) => {
+    return typeof a
+}
+
+console.log(myFunction(1))
+
+
+
---
+++ Fundamentales/fundamentales6.js
@@ -0,0 +1,11 @@
+/*Ejercicio 6:
+Escribe una función que tome una cadena como argumento. Extraiga los últimos 3 caracteres de la cadena. Devolver el resultado
+*/
+const myFunction = (str) => {
+    return str.slice(0, 3)
+}
+
+console.log(myFunction('abcdefg'))
+
+
+
